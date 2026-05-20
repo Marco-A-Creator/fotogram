9c9ea3e07c1e9f7e775ff7faf3b58d39d9fe0563
Marco-A-Creator <abbtmarco@gmx.de>
2026-05-20 10:24:22 +0200

    img renames and first script lines

--- script/script.js
+++ script/script.js
@@ -1,1 +1,21 @@
+const dialogRef = document.getElementById("photo-dialog");
+const photoIndex = document.getElementById("photo-album");
+
+//*Img Array
+const photos = [
+    "01snowmountain.jpg",
+    "02animecity.jpg",
+    "03stormclouds.png",
+    "04birdontree.jpg",
+    "05huricane.jpg",
+    "06alsosnowemountain.jpg",
+    "07duck.jpg",
+    "08standingguy.jpg",
+    "09birdontherocks.jpg",
+    "10cutiwildcats.jpg",
+    "11bluesky.jpg",
+    "12snowtree.jpg",
+];
+
+let total = photos.length;
 
